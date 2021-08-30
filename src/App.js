143db38f826cd2adc2382668167d4a205022bbe0
jrguedelha <jrguedelha@gmail.com>
2021-08-30 09:04:57 -0300

    Alterações no Header.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
+import Header from "./Header"
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
           <Route path="/">
 
+            <Header />
             <Home />
 
           </Route>
