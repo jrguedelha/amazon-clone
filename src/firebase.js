68affce2a0d5c4ea9ce3fde337f6fdc7d1766168
jrguedelha <jrguedelha@gmail.com>
2021-08-31 17:20:51 -0300

    Configuração do Firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-9BOCN8OD1SJCrHqc_VKmNQd80b7sFW8",
+  authDomain: "amz-clone-medium-42305.firebaseapp.com",
+  projectId: "amz-clone-medium-42305",
+  storageBucket: "amz-clone-medium-42305.appspot.com",
+  messagingSenderId: "898631127165",
+  appId: "1:898631127165:web:53a553cdb402b596f8e114",
+  measurementId: "G-CH22VG5NP7"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
