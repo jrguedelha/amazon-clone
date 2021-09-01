e99dfb93840ff6df42fdc2f307208ee81a87967e
jrguedelha <jrguedelha@gmail.com>
2021-09-01 11:23:31 -0300

    Funcionalidades da tela de Login

--- src/Login.js
+++ src/Login.js
@@ -5,8 +5,34 @@ import { auth } from "./firebase";
 
 function Login() {
 
+	const history = useHistory();
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+
+	const signIn = e => {
+		e.preventDefault();
+
+		auth
+			.signInWithEmailAndPassword(email, password)
+			.then(auth => {
+				history.push('/')
+			})
+			.catch(error => alert(error.message))
+	}
+
+	const register = e => {
+		e.preventDefault();
+
+		auth
+			.createUserWithEmailAndPassword(email, password)
+			.then((auth) => {
+				// it successfully created a new user with email and password
+				if (auth) {
+					history.push('/')
+				}
+			})
+			.catch(error => alert(error.message))
+	}
 
 	return (
 		<div className='login'>
@@ -35,7 +61,7 @@ function Login() {
 						onChange={e => setPassword(e.target.value)}
 					/>
 
-					<button type='submit' className='login__signInButton'>Entrar</button>
+					<button type='submit' onClick={signIn} className='login__signInButton'>Entrar</button>
 				</form>
 
 				<p>
@@ -43,7 +69,7 @@ function Login() {
 					Por favor verifique nossa política de privacidade. 
 				</p>
 
-			<button className='login__registerButton'>Crie sua conta na Amazon</button>
+			<button onClick={register} className='login__registerButton'>Crie sua conta na Amazon</button>
 
 			</div>
 
