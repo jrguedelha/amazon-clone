27f4f77dd1663dcbdf908a2aedc6230821b79245
jrguedelha <jrguedelha@gmail.com>
2021-09-01 20:31:40 -0300

    Adicionado CheckoutProduct

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./CheckoutProduct.css"
+import { useStateValue } from "./StateProvider";
+
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
+	const [{ basket }, dispatch] = useStateValue();
+
+	const removeFromBasket = () => {
+		// remove items from the basket
+		dispatch({
+			type: 'REMOVE_FROM_BASKET',
+			id: id
+		});
+	}
+
+	return(
+		<div className="checkoutProduct">
+			<img className="checkoutProduct__image" src={image} />
+
+			<div className="checkoutProdut__info">
+				<p className="checkoutProduct__title">{title}</p>
+				<p className="checkoutProcuct__price">
+					<small>R$</small>
+					<strong>{price}</strong>
+				</p>
+
+				<div className="checkoutProduct__rating">
+					{Array(rating)
+						.fill()
+						.map((_, i) => (
+							<p>🌟</p>
+						))
+					}	
+				</div>
+
+				{!hideButton && (
+					<button onClick={removerFromBasket}>
+						Remover da cesta
+					</button>
+				)}
+
+			</div>
+
+		</div>
+	);
+}
